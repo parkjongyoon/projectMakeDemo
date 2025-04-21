11b695dfb9b6235f7db0963257c8ee06ec733a11
박종윤님/Next Service <P203051@sktelecom.com>
2025-04-21 14:07:23 +0900

    init commit

---
+++ src/main/java/com/skt/ProjectMakeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.skt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMakeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMakeDemoApplication.class, args);
+	}
+
+}
